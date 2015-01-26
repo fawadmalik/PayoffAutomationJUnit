e6a0a2c2316e1a25ab36f1fbff27fb6002433466
michaelthecsguy <myang@ca02mlst11196.attlocal.net>
2015-01-25 21:53:28 -0800

    First Commit to First SE Webdriver automation
    SE Webdriver with jUnit

---
+++ src/test/java/com/payoff/AppTest.java
@@ -0,0 +1,209 @@
+package com.payoff;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase
+{
+    private WebDriver driver;
+    private Map<String,String> baseURLs;
+
+    public AppTest()
+    {
+        baseURLs = new HashMap<>();
+
+        baseURLs.put("google", "http://www.google.com");
+        baseURLs.put("payoff", "http://www.payoff.com");
+
+    }
+
+    public void setUp() throws Exception
+    {
+
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    public void tearDown() throws Exception
+    {
+        this.driver.quit();
+    }
+
+    public void testSeleniumSetup() throws Exception {
+        this.driver.get(baseURLs.get("google"));
+        assertEquals("Google", this.driver.getTitle());
+    }
+
+    public void testPayOffSiteUp() throws Exception
+    {
+        String siteName = "Payoff";
+        this.driver.get(baseURLs.get("payoff"));
+        assert(this.driver.getTitle().contains(siteName));
+    }
+
+    //one positive
+    public void testPayOffScreenWithValidDataSubmission() throws Exception
+    {
+        WebDriverWait poScreenWait = new WebDriverWait(this.driver, 3);
+        this.driver.get(baseURLs.get("payoff"));
+        poScreenWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("po-screen-widget")));
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //for the click and page directs
+
+        WebElement loanAmountInputElement;
+        WebElement creditScoreInputElement;
+        Select creditSelectDropDown;
+        WebElement whatWouldYouDoInputElement;
+        WebElement applyNowButton;
+
+        if (isElementPresent(By.id("po-screen-widget")) && isElementPresent(By.id("btn-submit-po-screen-widget")))
+        {
+            loanAmountInputElement = driver.findElement(By.id("loan_amount-po-screen-widget"));
+            loanAmountInputElement.sendKeys("$5,000");
+            //System.out.println(inputElement.getAttribute("value"));
+
+            creditScoreInputElement = driver.findElement(By.id("credit_score_bracket-po-screen-widget"));
+            creditSelectDropDown = new Select(creditScoreInputElement);
+            creditSelectDropDown.selectByValue("fair");
+
+            whatWouldYouDoInputElement = driver.findElement(By.id("introspection-po-screen-widget"));
+            whatWouldYouDoInputElement.sendKeys("I will feel FREEDOM.");
+
+            applyNowButton = driver.findElement(By.id("btn-submit-po-screen-widget"));
+
+            assertEquals("$5,000", loanAmountInputElement.getAttribute("value"));
+            assertEquals("fair", creditScoreInputElement.getAttribute("value"));
+            assertEquals("I will feel FREEDOM.", whatWouldYouDoInputElement.getAttribute("value"));
+
+            applyNowButton.click();
+
+            //Thread.sleep(5000);
+            WebDriverWait waitForPrequalificationPage = new WebDriverWait(driver, 5L);
+            waitForPrequalificationPage.until(ExpectedConditions.visibilityOfElementLocated(By.id("btn-submit")));
+
+            //https://apply.payoff.com/b9d5c66d-3a1c-49d8-8b8c-b04cba6532c4/#/pre-qualification
+            assertEquals(this.driver.getTitle(), "Payoff - Pre Qualification");
+            assert(this.isElementPresent(By.id("eca")));
+            assert(this.isElementPresent(By.id("btn-submit")));
+            assert(driver.getCurrentUrl().contains("pre-qualification"));
+        }
+        else
+            assert(false);
+
+    }
+
+    //one positive: Input with invalid data
+    public void testPayOffScreenWithInvalidData()
+    {
+        WebDriverWait poScreenWait = new WebDriverWait(this.driver, 3);
+        this.driver.get(baseURLs.get("payoff"));
+        poScreenWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("po-screen-widget")));
+
+        WebElement loanAmountInputElement;
+        WebElement creditScoreInputElement;
+        Select creditSelectDropDown;
+        WebElement whatWouldYouDoInputElement;
+        WebElement applyNowButton;
+
+        if (isElementPresent(By.id("po-screen-widget")) && isElementPresent(By.id("btn-submit-po-screen-widget")))
+        {
+            loanAmountInputElement = driver.findElement(By.id("loan_amount-po-screen-widget"));
+            loanAmountInputElement.sendKeys("");
+
+            creditScoreInputElement = driver.findElement(By.id("credit_score_bracket-po-screen-widget"));
+            creditSelectDropDown = new Select(creditScoreInputElement);
+            creditSelectDropDown.selectByValue("not_sure");
+
+            whatWouldYouDoInputElement = driver.findElement(By.id("introspection-po-screen-widget"));
+            whatWouldYouDoInputElement.sendKeys("I will feel FREEDOM.");
+
+            applyNowButton = driver.findElement(By.id("btn-submit-po-screen-widget"));
+
+            assertEquals("not_sure", creditScoreInputElement.getAttribute("value"));
+            assertEquals("I will feel FREEDOM.", whatWouldYouDoInputElement.getAttribute("value"));
+
+            //System.out.println(loanAmountInputElement.getAttribute("value"));
+            if (loanAmountInputElement.getAttribute("value").equals(""))
+            {
+                if (this.isElementPresent(By.cssSelector("div.text-danger")))
+                {
+                    String actualErrorMsg = driver.findElement(By.cssSelector("div.text-danger")).getText();
+                    assertEquals("Please enter a value between $5,000 to $25,000", actualErrorMsg);
+                    assert(!applyNowButton.isEnabled());
+                }
+                else
+                    assert(false);
+            }
+            else
+                assert(false);
+        }
+        else
+            assert(false);
+
+    }
+
+    //one negative: Submit
+    public void testPayOffScreenSubmissionWithInvalidData()
+    {
+        WebDriverWait poScreenWait = new WebDriverWait(this.driver, 3);
+        this.driver.get(baseURLs.get("payoff"));
+        poScreenWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("po-screen-widget")));
+
+        WebElement loanAmountInputElement;
+        WebElement creditScoreInputElement;
+        Select creditSelectDropDown;
+        WebElement whatWouldYouDoInputElement;
+        WebElement applyNowButton;
+
+        if (isElementPresent(By.id("po-screen-widget")) && isElementPresent(By.id("btn-submit-po-screen-widget")))
+        {
+            loanAmountInputElement = driver.findElement(By.id("loan_amount-po-screen-widget"));
+            loanAmountInputElement.sendKeys("");
+
+            creditScoreInputElement = driver.findElement(By.id("credit_score_bracket-po-screen-widget"));
+            creditSelectDropDown = new Select(creditScoreInputElement);
+            creditSelectDropDown.selectByValue("0");
+
+            whatWouldYouDoInputElement = driver.findElement(By.id("introspection-po-screen-widget"));
+            whatWouldYouDoInputElement.sendKeys("I will feel FREEDOM.");
+
+            applyNowButton = driver.findElement(By.id("btn-submit-po-screen-widget"));
+
+            applyNowButton.click();
+            assert(this.isElementPresent(By.cssSelector("div.text-danger")));
+            System.out.println(this.driver.getTitle());
+            assert(this.driver.getTitle().contains("Refinancing Credit Card Debt"));
+        }
+        else
+            assert(false);
+
+    }
+
+    //convenient method
+    public boolean isElementPresent(By by)
+    {
+        try
+        {
+            driver.findElements(by);
+            return true;
+        }
+        catch (NoSuchElementException e)
+        {
+            return false;
+        }
+    }
+}
